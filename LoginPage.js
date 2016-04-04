aa7b810af6093127581b376fc0add80c2434c969
Kyle Bolton <kbolton@twitter.com>
2016-04-04 12:28:18 -0400

    Automatically log the user in if a digits session exists

--- LoginPage.js
+++ LoginPage.js
@@ -4,6 +4,7 @@ var React = require('react-native');
 
 var Digits = require('react-native-fabric-digits');
 var { DigitsLoginButton, DigitsLogoutButton } = Digits;
+var DigitsManager = require("react-native").NativeModules.DigitsManager;
 
 var {
   AppRegistry,
@@ -46,6 +47,13 @@ class LoginPage extends Component {
     };
   }
 
+  componentDidMount() {
+    DigitsManager.session((session) => {
+      console.log(session)
+      this.setState({logged: !!session});
+    });
+  }
+
   completion(error, response) {
     if (error && error.code !== 1) {
       this.setState({ logged: false, error: true, response: {} });
@@ -59,54 +67,62 @@ class LoginPage extends Component {
   }
 
   render() {
-    var error = this.state.error ? <Text>An error occured.</Text> : null;
-    var content = this.state.logged ?
-      (<View>
-        <Text>
-          Auth Token: {this.state.response.authToken}{'\n'}
-          Auth Token Secret: {this.state.response.authTokenSecret}{'\n\n'}
-        </Text>
-        <DigitsLogoutButton
-       completion={this.completion.bind(this)}
-          text="Logout"
-          buttonStyle={styles.DigitsAuthenticateButton}
-          textStyle={styles.DigitsAuthenticateButtonText}/>
-      </View>) : (<DigitsLoginButton
-        options={{
-          title: "Talking Stick Login",
-          phoneNumber: "+1",
-          appearance: {
-            backgroundColor: {
-              hex: "#ffffff",
-              alpha: 1.0
-            },
-            accentColor: {
-              hex: "#43a16f",
-              alpha: 0.7
-            },
-            headerFont: {
-              name: "Arial",
-              size: 16
-            },
-            labelFont: {
-              name: "Helvetica",
-              size: 18
-            },
-            bodyFont: {
-              name: "Helvetica",
-              size: 16
-            }
-          }
-        }}
-        completion={this.completion.bind(this)}
-        text="Login"
-        buttonStyle={styles.DigitsAuthenticateButton}
-        textStyle={styles.DigitsAuthenticateButtonText}/>);
     return (
       <View style={styles.container}>
-        {error}
-        {content}
+        {this.state.error ? <Text>An error occured.</Text> : null}
+        {this.state.logged ? this.renderLoggedIn() : this.renderLoginScreen()}
       </View>
+    );
+  }
+
+  renderLoggedIn() {
+    return (
+      <View>
+      <Text>
+      Auth Token: {this.state.response.authToken}{'\n'}
+      Auth Token Secret: {this.state.response.authTokenSecret}{'\n\n'}
+      </Text>
+      <DigitsLogoutButton
+      completion={this.completion.bind(this)}
+      text="Logout"
+      buttonStyle={styles.DigitsAuthenticateButton}
+      textStyle={styles.DigitsAuthenticateButtonText}/>
+      </View>
+    );
+  }
+  renderLoginScreen() {
+    return (
+      <DigitsLoginButton
+      options={{
+        title: "Talking Stick Login",
+        phoneNumber: "+1",
+        appearance: {
+          backgroundColor: {
+            hex: "#ffffff",
+            alpha: 1.0
+          },
+          accentColor: {
+            hex: "#43a16f",
+            alpha: 0.7
+          },
+          headerFont: {
+            name: "Arial",
+            size: 16
+          },
+          labelFont: {
+            name: "Helvetica",
+            size: 18
+          },
+          bodyFont: {
+            name: "Helvetica",
+            size: 16
+          }
+        }
+      }}
+      completion={this.completion.bind(this)}
+      text="Login"
+      buttonStyle={styles.DigitsAuthenticateButton}
+      textStyle={styles.DigitsAuthenticateButtonText}/>
     );
   }
 }
